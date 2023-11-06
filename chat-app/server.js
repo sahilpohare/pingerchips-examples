5606005c1be73325f484ba174b7418a4544d25fa
rahul81 <rahuldas.rr81@gmail.com>
2023-11-06 21:31:14 +0530

    updated chat app server.js

--- chat-app/server.js
+++ chat-app/server.js
@@ -1,10 +1,7 @@
-const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const bodyParser = require('body-parser');
 
 app.use(express.static("public"));
-app.use(bodyParser.json());
 
 http.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
